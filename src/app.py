101255917b3a4246a9d6bccb0b89d2b040685836
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-02 17:03:18 +1100

    Add data dict

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '6ba6b74d871b12dea79bfc9013067c7c'
 
-content = [
+form_content = [
     {
         'title' : 'web development',
         'question' : 'Quality and value of user inferfaces.'
@@ -20,13 +20,18 @@ content = [
 def home():
     user_form = UserInfoForm()
     if user_form.validate_on_submit():
-        return redirect(url_for('form'))
-    return render_template('home.html', user_form=user_form, content=content)
+        data = {
+            'name' : user_form.project_name.data,
+            'project_name' : user_form.project_name.data,
+            'project_category' : user_form.project_category_name.data
+        }
+        return redirect(url_for('form', data=data))
+    return render_template('home.html', user_form=user_form, form_content=form_content)
 
 @app.route('/home/form')
 def form():
     feedback_form = FeedbackForm()
-    return render_template('form.html', feedback_form=feedback_form, content=content)
+    return render_template('form.html', feedback_form=feedback_form, form_content=form_content)
 
 if __name__ == '__main__':
     app.run(debug=True)
