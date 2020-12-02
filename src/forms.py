f8f33f6910d62b3ef198b1148e2bed98d16fcb7d
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-02 15:58:31 +1100

    Add two more fields

--- src/forms.py
+++ src/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class UserInfoForm(FlaskForm):
     individual_name = StringField('Individual name: ', validators=[DataRequired()])
+    project_name = StringField('Project name: : ', validators=[DataRequired()])
+    project_category_name =SelectField('Project category: ', validators=[DataRequired()])    
+    submit = SubmitField('continue')
