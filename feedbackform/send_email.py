331df2f60b04e0f3d0d7412872ff4b7ec8132ae6
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-03 21:06:19 +1100

    Initial commit

---
+++ feedbackform/send_email.py
@@ -0,0 +1,21 @@
+import smtplib
+from feedbackform.static_config_parser import StaticConfigParser
+
+config = StaticConfigParser()
+EMAIL_ADDRESS = config.get('EMAIL', 'email_address')
+EMAIL_PASSWORD = config.get('EMAIL', 'email_password')
+
+# close automatically
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo() # identify ourselves with gmail
+    smtp.starttls() # encrypt traffic
+    smtp.ehlo() # re-identify ourselves as encrypted traffic
+
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    subject = "hello there"
+    body = "epic body paragraph"
+
+    message = f"Subject: {subject}\n\n{body}"
+
+    smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message) # email ourselves 
