9ebda678635ecb8b36ecfd9494454ed41f982b69
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-03 23:49:15 +1100

    Add comment

--- feedbackform/forms.py
+++ feedbackform/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, RadioField
 from wtforms.validators import DataRequired
 
+# Add a "any more comments?" field
+
 class UserInfoForm(FlaskForm):
     individual_name = StringField('Individual name: ', validators=[DataRequired()])
     business_name = StringField('Business name: ', validators=[DataRequired()])
