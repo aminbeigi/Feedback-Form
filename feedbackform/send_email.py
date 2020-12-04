603ccf5cc5a68a6a34e32f4520bab3adac0fd93a
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-04 11:40:32 +1100

    Delete spaces

--- feedbackform/send_email.py
+++ feedbackform/send_email.py
@@ -7,13 +7,12 @@ EMAIL_PASSWORD = config.get('EMAIL', 'email_password')
 
 class SendEmail():
     def send(self, subject, body):
+        # quits automatically
         with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
             smtp.ehlo() # identify ourselves with gmail
-            smtp.starttls() # encrypt traffic
+            smtp.starttls() # put SMTP connection in TLS mode
             smtp.ehlo() # re-identify ourselves as encrypted traffic
-
+            
             smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-
             message = f"Subject: {subject}\n\n{body}"
-
             smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message) # email ourselves
